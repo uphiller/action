b5bf5e3b183507dec1253cf3f04976476be3df22
youngho <jason.uphiller@gmail.com>
2024-03-05 12:51:29 +0900

    update git action script

--- src/test/java/com/example/action/ActionApplicationTests.java
+++ src/test/java/com/example/action/ActionApplicationTests.java
@@ -1,13 +1,111 @@
 package com.example.action;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.rds.AmazonRDS;
+import com.amazonaws.services.rds.AmazonRDSClientBuilder;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.util.IOUtils;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class ActionApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        List<List<String>> list = getExcel();
+        List<List<String>> ec2List = list.stream().filter( e -> e.get(0).equals("ec2") || e.get(0).equals("ebs")).collect(Collectors.toList());
+        List<List<String>> rdsList = list.stream().filter( e -> e.get(0).equals("rds")).collect(Collectors.toList());
+
+        AmazonEC2 ec2Client = AmazonEC2ClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider("insw-dev"))
+                .build();
+
+        for(List<String> resource : ec2List) {
+            setEc2Tag(resource, ec2Client);
+        }
+
+        AmazonRDS rdsClient = AmazonRDSClientBuilder.standard()
+                .withRegion("ap-northeast-2")
+                .withCredentials(new ProfileCredentialsProvider("insw-dev"))
+                .build();
+
+        for(List<String> resource : rdsList) {
+            setRdsTag(resource, rdsClient);
+        }
+
+    }
+
+    public static List<List<String>> getExcel() throws IOException {
+        List<List<String>> list = new ArrayList();
+
+        try {
+            FileInputStream file = new FileInputStream("C:\\Users\\youngho\\Downloads\\tags.xlsx");
+            IOUtils.setByteArrayMaxOverride(Integer.MAX_VALUE);
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                List<String> data = new ArrayList();
+                int columnCnt = 0;
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+
+                    String code = "";
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            data.add(cell.getStringCellValue());
+                            break;
+                        default:
+                            data.add("");
+                    }
+                    columnCnt++;
+
+                    if(columnCnt > 2) break;
+                }
+
+                list.add(data);
+            }
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public static void setEc2Tag(List<String> data, AmazonEC2 ec2Client){
+//        Tag tag = new Tag().withKey("AppOwner").withValue(data.get(2));
+//        CreateTagsRequest createTagsRequest = new CreateTagsRequest()
+//                .withResources(data.get(1))
+//                .withTags(tag);
+//
+//        ec2Client.createTags(createTagsRequest);
+        System.out.println(data);
+    }
+
+    public static void setRdsTag(List<String> data, AmazonRDS rdsClient){
+//        com.amazonaws.services.rds.model.Tag tag = new com.amazonaws.services.rds.model.Tag().withKey("AppOwner").withValue(data.get(2));
+//        AddTagsToResourceRequest request = new AddTagsToResourceRequest()
+//                .withResourceName(data.get(1))
+//                .withTags(tag);
+//
+//        rdsClient.addTagsToResource(request);
+        System.out.println(data);
     }
 
 }
---
+++ terraform/eb.tf
@@ -0,0 +1,187 @@
+resource "aws_vpc" "pay_vpc" {
+  cidr_block = "172.30.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "iaw-clo-ss-vpc-an2-dev-1"
+  }
+}
+
+resource "aws_internet_gateway" "pay_igw" {
+  vpc_id = aws_vpc.pay_vpc.id
+
+  tags = {
+    Name = "iaw-clo-ss-igw-an2-dev-1"
+  }
+}
+
+resource "aws_route" "pay_route" {
+  route_table_id         = aws_vpc.pay_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"  # Default route
+  gateway_id             = aws_internet_gateway.pay_igw.id
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.pay_vpc.id
+  cidr_block              = "172.30.1.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "iaw-clo-ss-public_subnet-an2-dev-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.pay_vpc.id
+  cidr_block              = "172.30.2.0/24"
+  availability_zone = "ap-northeast-2b"
+
+  tags = {
+    Name = "iaw-clo-ss-public_subnet-an2-dev-1"
+  }
+}
+
+resource "aws_elastic_beanstalk_application" "my_app" {
+  name = "iaw-clo-ss-eb_pay-an2-dev-1"
+}
+
+resource "aws_elastic_beanstalk_environment" "pay" {
+  name                = "iaw-clo-ss-eb-pay-an2-dev-1"
+  application         = aws_elastic_beanstalk_application.my_app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.2.2 running Docker"
+  tier                = "WebServer"
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = aws_vpc.pay_vpc.id
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id])
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     =  "True"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = "1"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = "2"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "LoadBalanced"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t3.micro"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "aws-elasticbeanstalk-service-role"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application"
+    name      = "Application Healthcheck URL"
+    value     = "/"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internet facing"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "EC2KeyName"
+    value     = "younghokwak"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "Port"
+    value     = "8080"
+  }
+
+  setting {
+    name = "Unit"
+    namespace = "aws:autoscaling:trigger"
+    value = "Percent"
+  }
+  setting {
+    name = "MeasureName"
+    namespace = "aws:autoscaling:trigger"
+    value = "CPUUtilization"
+  }
+  setting {
+    name = "LowerThreshold"
+    namespace = "aws:autoscaling:trigger"
+    value = "20"
+  }
+  setting {
+    name = "UpperThreshold"
+    namespace = "aws:autoscaling:trigger"
+    value = "90"
+  }
+  setting {
+    name = "Period"
+    namespace = "aws:autoscaling:trigger"
+    value = "5"
+  }
+  setting {
+    name = "UpperBreachScaleIncrement"
+    namespace = "aws:autoscaling:trigger"
+    value = "1"
+  }
+  setting {
+    name = "LowerBreachScaleIncrement"
+    namespace = "aws:autoscaling:trigger"
+    value = "-1"
+  }
+
+}
