0a4d7f86264d2777a0f7a5fdf203f822ef49ae8f
youngho <jason.uphiller@gmail.com>
2024-03-06 15:03:48 +0900

    modify tag code

--- src/test/java/com/example/action/ActionApplicationTests.java
+++ src/test/java/com/example/action/ActionApplicationTests.java
@@ -3,10 +3,14 @@ package com.example.action;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
+import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.services.rds.AmazonRDS;
 import com.amazonaws.services.rds.AmazonRDSClientBuilder;
+import com.amazonaws.services.rds.model.AddTagsToResourceRequest;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.util.IOUtils;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -21,7 +25,7 @@ import java.util.stream.Collectors;
 
 class ActionApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() throws IOException {
         List<List<String>> list = getExcel();
         List<List<String>> ec2List = list.stream().filter( e -> e.get(0).equals("ec2") || e.get(0).equals("ebs")).collect(Collectors.toList());
@@ -30,7 +34,6 @@ class ActionApplicationTests {
         AmazonEC2 ec2Client = AmazonEC2ClientBuilder.standard()
                 .withCredentials(new ProfileCredentialsProvider("insw-dev"))
                 .build();
-
         for(List<String> resource : ec2List) {
             setEc2Tag(resource, ec2Client);
         }
@@ -65,17 +68,19 @@ class ActionApplicationTests {
                 while (cellIterator.hasNext()) {
                     Cell cell = cellIterator.next();
 
-                    String code = "";
                     switch (cell.getCellType()) {
                         case STRING:
                             data.add(cell.getStringCellValue());
+                            break;
+                        case NUMERIC:
+                            data.add(NumberToTextConverter.toText((cell.getNumericCellValue())));
                             break;
                         default:
                             data.add("");
                     }
                     columnCnt++;
 
-                    if(columnCnt > 2) break;
+                    if(columnCnt > 3) break;
                 }
 
                 list.add(data);
@@ -89,23 +94,76 @@ class ActionApplicationTests {
     }
 
     public static void setEc2Tag(List<String> data, AmazonEC2 ec2Client){
-//        Tag tag = new Tag().withKey("AppOwner").withValue(data.get(2));
-//        CreateTagsRequest createTagsRequest = new CreateTagsRequest()
-//                .withResources(data.get(1))
-//                .withTags(tag);
-//
-//        ec2Client.createTags(createTagsRequest);
-        System.out.println(data);
+        try {
+            if (data.size() > 2) {
+                Tag appOwnerCd = new Tag().withKey("AppOwnerCd").withValue(data.get(3));
+                Tag appOwnerNm = new Tag().withKey("AppOwnerNm").withValue(data.get(2));
+                CreateTagsRequest createTagsRequest = new CreateTagsRequest()
+                        .withResources(data.get(1))
+                        .withTags(appOwnerCd, appOwnerNm);
+
+                ec2Client.createTags(createTagsRequest);
+            }
+        }catch(Exception e){
+        }
     }
 
     public static void setRdsTag(List<String> data, AmazonRDS rdsClient){
-//        com.amazonaws.services.rds.model.Tag tag = new com.amazonaws.services.rds.model.Tag().withKey("AppOwner").withValue(data.get(2));
-//        AddTagsToResourceRequest request = new AddTagsToResourceRequest()
-//                .withResourceName(data.get(1))
-//                .withTags(tag);
-//
-//        rdsClient.addTagsToResource(request);
-        System.out.println(data);
+        try {
+            if (data.size() > 2) {
+                com.amazonaws.services.rds.model.Tag appOwnerCd = new com.amazonaws.services.rds.model.Tag().withKey("AppOwnerCd").withValue(data.get(3));
+                com.amazonaws.services.rds.model.Tag appOwnerNm = new com.amazonaws.services.rds.model.Tag().withKey("AppOwnerNm").withValue(data.get(2));
+                AddTagsToResourceRequest request = new AddTagsToResourceRequest()
+                        .withResourceName(data.get(1))
+                        .withTags(appOwnerCd, appOwnerNm);
+
+                rdsClient.addTagsToResource(request);
+            }
+        }catch(Exception e){
+        }
+    }
+
+//    @Test
+    void getTeamCode() throws IOException {
+        List<List<String>> list = new ArrayList();
+
+        try {
+            FileInputStream file = new FileInputStream("C:\\Users\\youngho\\Downloads\\team.xlsx");
+            IOUtils.setByteArrayMaxOverride(Integer.MAX_VALUE);
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                List<String> data = new ArrayList();
+                int columnCnt = 0;
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            if(columnCnt == 0 || columnCnt == 1) data.add(cell.getStringCellValue());
+                            break;
+                        default:
+                            data.add("");
+                    }
+                    columnCnt++;
+                }
+
+                list.add(data);
+            }
+            file.close();
+
+            for(List<String> d: list){
+                System.out.println(d);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
